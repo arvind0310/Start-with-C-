eb00238d88c7bd8d8f4067f40fb88931ad4a59e1
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-17 17:54:20 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,7 +6,7 @@ main ()
   int n;
   cin >> n;
   int i;
-  for (int i = 2; i < n; i++)
+  for (int i = 2; i < n; i++) 
     {
       if (n % i == 0) // will enter this loop when condition will be true ..else not(out of loop)
 	{
@@ -25,6 +25,52 @@ main ()
     
     }
   if (i == n) // did it competed the all iteration?
+    {
+      cout<<"prime no "<<endl;
+    }
+
+
+
+  return 0;
+}
+
+
+
+
+//OR//
+
+
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int
+main ()
+{
+  int n;
+  cin >> n;
+  bool flag=0;
+  int i;
+  for (int i = 2; i < sqrt(n); i++) 
+    {
+      if (n % i == 0) // will enter this loop when condition will be true ..else not(out of loop)
+	{
+	  cout << "not a prime no" << endl;
+	  break;
+	  flag=1;
+	}
+	
+	/*   wrong  ('55' test case fail  ) if will enter immidiately on else when if condition false [we cant conclude it is prime untill all iteration is finished]*/ 
+	
+//       else 
+//       {
+//           cout<<"prime no "<<endl;
+//           break;
+//       }
+    
+    
+    }
+  if (flag==0) // did it competed the all iteration?
     {
       cout<<"prime no "<<endl;
     }
