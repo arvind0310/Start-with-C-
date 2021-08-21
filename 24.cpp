86f017d7c20c3b7322b181a50660473fda6a8157
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-22 00:28:29 +0530

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -36,3 +36,52 @@ for (int i=0;i<n+1;i++) // will run 5 times {n=4+1}
 }
 
 
+//___________ _____________________________________ ______________________________________  _________________________________ ____________________________
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+int arr[50];
+int n;
+cout<<"enter the size of array: "<<endl;
+cin>>n;
+
+cout<<"enter the"<<n<<" element<<endl;
+for ( int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+// int cap=sizeof(arr)/sizeof(arr[0]);
+int pos;
+cout<<"enter the position: "<<endl;
+cin>>pos;
+
+int x;
+cout<<"enter the inserting element"<<endl;
+cin>>x;
+
+
+// if(n==cap)
+// {
+//     cout<<"no space in arr";
+// }
+
+int idx=pos-1;
+for(int i=n-1;i>=idx;i--)
+{
+    arr[i+1]=arr[i];
+}
+arr[idx]=x;
+
+cout<<"new array is:"<<endl;
+for (int i=0;i<n+1;i++) // will run 5 times {n=4+1}
+{
+    cout<<arr[i]<<" ";
+}
+
+  return 0;  
+}
