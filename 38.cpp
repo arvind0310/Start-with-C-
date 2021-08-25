7d8ef1e26f67ca6262c8b97ace56a704de3969e1
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-26 01:08:42 +0530

    Update 38.cpp

--- 38.cpp
+++ 38.cpp
@@ -35,7 +35,9 @@ int main ()
 //   int arr[] = {3,4,5,6,7,8,9};
 
 
-  int arr[]={1,2,3,4,5};
+//   int arr[]={1,2,3,4,5};
+   
+     int arr[]={1,2,3,4,5,6};
 
   int n = sizeof (arr) / sizeof (arr[0]);
   cout << Binsearch(arr, n,2);
