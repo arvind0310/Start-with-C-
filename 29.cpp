aa5cc2246275da441b0b293793f1a557bf2300be
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-25 20:57:04 +0530

    Update 29.cpp

--- 29.cpp
+++ 29.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int rev(int arr[],int n)
 {
     int low=0,high=n-1;
-    while(low<high)
+    while(low<=high)
     {
       int temp;      // swap program
       temp=arr[low];
@@ -45,7 +45,7 @@ using namespace std;
 void rev(int arr[],int n)
 {
     int low=0,high=n-1;
-    while(low<high)
+    while(low<=high)
     {
       int temp;   // swap program
       temp=arr[low];
