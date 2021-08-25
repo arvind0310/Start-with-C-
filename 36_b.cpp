2db1bb7780027ecb13d2559c981c9f3b4aa9ef02
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-25 20:21:03 +0530

    Create 36_b.cpp

---
+++ 36_b.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maxksum(int arr[], int n, int k)	// you can use int arr[] will get same ans
+{
+
+int curr_sum=0;
+for(int i=0;i<k;i++)
+{
+    curr_sum+=arr[i];   // sum of first window
+}
+  
+int max_sum=curr_sum;
+
+  // for next windows ..
+  
+for(int i=k;i<n;i++)
+{
+    curr_sum+=(arr[i]-arr[i-k]);  //adding next element and removing 1st element of previous window to the curr_sum..(sum of prev window)
+    max_sum=max(curr_sum,max_sum); // next window sum is comapred with first one and update if changed 
+}
+
+  return max_sum;
+}
+
+
+int
+main ()
+{
+
+//   int arr[] = { 1, 2, 3, 4, 5 };
+
+    int arr[] = {1,8,30,-5,20,7};
+
+  // int arr[] = {-1,-2,-3,-4,-5}; thats why INT_MIN was taken(-infi) instead of 0, so that we can compare list to list min value.otherwise ans would be zero. 
+
+  int n = sizeof (arr) / sizeof (arr[0]);
+  cout << maxksum(arr, n, 3);
+
+  return 0;
+}
